f1fad8ae818848959c01d703a9b5f91d27bd15a6
Imre <nemethi@kkszki.hu>
2026-02-24 20:07:30 +0100

    Views.cs létrehozása, sima kiírás

---
+++ FogadoOra/Models/Views.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FogadoOra.Models
+{
+    internal class Views
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Megjelenítés");
+        }
+
+
+        public void MegjelenitOra(List<FogadoOra> orak)
+        {
+            Console.WriteLine("Fogadó órák");
+            foreach(FogadoOra ora in orak)
+            {
+                Console.WriteLine(ora.Place);
+                Console.WriteLine(ora.Start);
+                Console.WriteLine(ora.Lenght);
+
+            }
+        }
+
+    }
+}

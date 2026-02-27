02a0da5f7d01a7fdd69dbcbd92398ff1a30fad88
Imre <nemethi@kkszki.hu>
2026-02-27 10:25:44 +0100

    kis hiba javítás

--- FogadoOra/View/Megjelenites.cs
+++ FogadoOra/View/Megjelenites.cs
@@ -280,7 +280,7 @@ namespace FogadoOra.View
 
         public void JelentkezesTorlese()
         {
-            IdAlapjanFogadoora();
+            new FogadoOraController().GetAllFogadoOraOfUser(currentUser.Id);
             Console.WriteLine("Adjon meg egy id-t!");
             int id = int.Parse(Console.ReadLine());
             new JelentkezesController().JelentkezesTorlese(id, currentUser.Id);
